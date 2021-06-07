17b03e3aaa5bcde4e3e0d183ede853e249abded7
Merida-Daniel <dacastamerd@gmail.com>
2021-06-06 23:37:22 -0500

    Se importo el React en app.js para inicializacion de pruebas

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { StoreProvider } from "./store";
+import List from "./lists/ListGroup";
+import Form from "./forms/FormGroup";
 
 function App() {
   return <StoreProvider>
