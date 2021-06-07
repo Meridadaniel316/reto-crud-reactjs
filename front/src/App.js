ecc94dad06f36c67b9147d686793f5f689e681be
Merida-Daniel <dacastamerd@gmail.com>
2021-06-06 23:45:23 -0500

    Se implemento el logo de SofkaU Training Ciclo 2

--- front/src/App.js
+++ front/src/App.js
@@ -5,11 +5,13 @@ import Form from "./forms/FormGroup";
 
 function App() {
   return <StoreProvider>
+  
     <div id="general">
       <h3>To-Do List</h3>
       <Form />
       <List />
     </div>
+    
   </StoreProvider>
 }
 
